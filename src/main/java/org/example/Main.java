39d8b77f881afbbee034ecbbe8bb0b0f5bc6ab67
arife-senol <avcisenolarife@gmail.com>
2024-07-30 15:03:45 +0300

    s14d2 project done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bedroom bedroom = new Bedroom("YatakOdası",new Wall("right"),new Wall("left"),new Wall("top"),new Wall("bottom"),new Ceiling(3, PaintColor.GREEN),new Bed("ortopedik",3,5,6,7),new Lamp(LampType.NEON,false,2),new Wardrobe(10,20,90.0),new Carpet(3,4,PaintColor.RED));
+
+
+        System.out.println(bedroom.getCarpet().getColor());
+        System.out.println(bedroom.getBed().getStyle());
+        System.out.println(bedroom.getName());
+
     }
 }
---
+++ src/main/java/org/example/model/Room.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+public class Room {
+    private String name;
+    private Wall wall1, wall2, wall3, wall4;
+    private Ceiling ceiling;
+
+    public Room(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling) {
+        this.name = name;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.ceiling = ceiling;
+    }
+
+    // Getter Methods
+    public String getName() {
+        return name;
+    }
+
+    public Wall getWall1() {
+        return wall1;
+    }
+
+    public Wall getWall2() {
+        return wall2;
+    }
+
+    public Wall getWall3() {
+        return wall3;
+    }
+
+    public Wall getWall4() {
+        return wall4;
+    }
+
+    public Ceiling getCeiling() {
+        return ceiling;
+    }
+}
